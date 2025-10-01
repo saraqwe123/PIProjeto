9095b1aad180e5202c61869ec70ec51b4d4b1f34
saraqwe123 <130059301+saraqwe123@users.noreply.github.com>
2025-10-01 11:06:58 -0300

    .

--- Frontend/src/pages/Login.jsx
+++ Frontend/src/pages/Login.jsx
@@ -3,24 +3,24 @@ import { Pagina } from "../components/Pagina";
 export function Login() {
   return (
     <Pagina>
-      <div className="flex flex-row w-full h-screen"> 
-
-        <div className="w-3/4 h-full">
-          <img src="/imagens/login.svg" alt="" className="h-full w-full"/>
+      <div className="flex flex-row w-full h-screen bg-[#2e2d2d]"> 
+        <div className="w-full h-screen flex flex-row">
+          <div className="w-2/3 h-9/10 bg-gradient-to-r from-[#0097b2] to-[#7ed957]">
+            <img src="public/imagens/loginApp.svg" alt=""/>
+          </div>
+          <div className="w-1/3 h-20/21 bg-[#2e2d2d] flex justify-center items-center">
+            <form action="" method="" className="w-90 h-19/21 bg-white rounded-2xl">
+              <label htmlFor="">Login</label>
+              <br/>
+              <input type="text"/>
+              <br/>
+              <label htmlFor="">Senha</label>
+              <br/>
+              <input type="text"/>
+            </form>
+          </div>
         </div>
-        <div>
-          <form action="" method="">
-            <label>Login</label>
-            <br/>
-            <input id="" name="" placeholder="Digite seu login..." />
-            <br/>
-            <label>Senha</label>
-            <br/>
-            <input id="" name="" placeholder="Digite sua senha..." />
-            <br/>
-            <button>Entrar</button>
-          </form>
-        </div>
+ 
       </div>
     </Pagina>
   );
