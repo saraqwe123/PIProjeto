0f83ecbea06e059bb77c22e058648af0d11535c9
Lara Deitos <130068472+laraheloisa@users.noreply.github.com>
2025-10-22 11:56:27 -0300

    area pix

--- Frontend/src/pages/AreaPix.jsx
+++ Frontend/src/pages/AreaPix.jsx
@@ -1,12 +1,24 @@
 import { Pagina } from "../components/Pagina";
 import { MoveUpRight } from "lucide-react";
+import { Key } from "lucide-react";
+import { Copy } from "lucide-react";
+import { DollarSign } from "lucide-react";
+import { CircleArrowLeft } from "lucide-react";
+import { CircleQuestionMark } from "lucide-react";
+
 
 export function AreaPix() {
   return (
     <Pagina>
       <div className="flex flex-col w-full h-screen">
         <div className="w-full h-20 bg-white flex justify-center items-center">
-          <button></button>
+          <div>
+            <button><CircleArrowLeft/></button>
+            <button><CircleQuestionMark/></button>
+          </div>
+          <div>
+
+          </div>
         </div>
 
         <div className="w-full flex-1 bg-[#259337] flex flex-col items-start p-10">
@@ -14,31 +26,34 @@ export function AreaPix() {
 
           <div className="flex gap-10 ml-10 flex-wrap">
             <div className="flex flex-col items-center">
-              <div className="w-50 h-50 bg-[#c1ff72] rounded-2xl shadow-md"><MoveUpRight className="w-20 h-40"/></div>
+              <div className="w-50 h-50 bg-[#c1ff72] rounded-2xl shadow-md flex justify-center items-center"><MoveUpRight className="w-20 h-50"/></div>
               <p className="text-white mt-2">Transferência</p>
             </div>
 
             <div className="flex flex-col items-center">
-              <div className="w-50 h-50 bg-[#5ce1e6] rounded-2xl shadow-md"></div>
+              <div className="w-50 h-50 bg-[#5ce1e6] rounded-2xl shadow-md flex justify-center items-center"><Key className="w-20 h-50" /></div>
               <p className="text-white mt-2">Mudar Chave</p>
             </div>
 
             <div className="flex flex-col items-center">
-              <div className="w-50 h-50 bg-gradient-to-r from-[#5170ff] to-[#ff66e4] rounded-2xl shadow-md"></div>
+              <div className="w-50 h-50 bg-gradient-to-r from-[#5170ff] to-[#ff66e4] rounded-2xl shadow-md flex justify-center items-center"><Copy className="w-20 h-50"/></div>
               <p className="text-white mt-2">Pix Copia e Cola</p>
             </div>
 
             <div className="flex flex-col items-center">
-             <div className="w-50 h-50 bg-gradient-to-r from-[#0097b2] to-[#7ed957] rounded-2xl shadow-md"></div>
+             <div className="w-50 h-50 bg-gradient-to-r from-[#0097b2] to-[#7ed957] rounded-2xl shadow-md flex justify-center items-center"><DollarSign className="w-20 h-50"/></div>
               <p className="text-white mt-2">Cobrar</p>
             </div>
           </div>
         </div>
 
+        <hr  className="h-1 bg-[#d9d9d9] border-[#d9d9d9]"/>
+
         <div className="w-full flex-1 bg-[#003c0a] flex justify-center items-center">
-          <p className="text-white text-lg"></p>
+          <p className="text-white text-lg">Histórico</p>
         </div>
       </div>
+
     </Pagina>
   );
 }
