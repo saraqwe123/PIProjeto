9b889f9c13656ed5a255e2f7948ae61706ca084b
saraqwe123 <130059301+saraqwe123@users.noreply.github.com>
2025-11-05 09:59:40 -0300

    responsividade do cadastro

--- Frontend/src/pages/Cadastro.jsx
+++ Frontend/src/pages/Cadastro.jsx
@@ -1,24 +1,52 @@
 import { Pagina } from "../components/Pagina";
 import { ArrowRight } from "lucide-react";
+import { useState } from "react"
 
 
 export function Cadastro() {
+  const [foto, setFoto] = useState(null);
+
+  const handleFotoChange = (e) => {
+    const file = e.target.files[0];
+    if (file) {
+      setFoto(URL.createObjectURL(file));
+    }
+  };
+
   return (
     <Pagina>
-      <div className="flex flex-row w-full h-screen bg-[#2e2d2d] fixed">
+      <div className="lg:flex flex-row w-full h-screen bg-[#2e2d2d] fixed">
         <div className="w-full h-screen flex flex-row">
-          <div className="w-2/3 h-9/10 bg-gradient-to-r from-[#0097b2] to-[#7ed957] flex flex-col justify-center items-center">
-            <img src="imagens/logoApp.svg" alt="" className="w-screen h-1/3" />
+          <div className="w-2/3 h-9/10 bg-gradient-to-r from-[#0097b2] to-[#7ed957] hidden lg:flex flex-col justify-center items-center">
+            <img src="imagens/logoApp.svg" alt="" className="w-screen h-1/3 hidden lg:block" />
             <br />
-            <p className="w-2/3 h-1/2 text-justify text-amber-50">
+            <p className="w-2/3 h-1/2 text-justify text-amber-50 hidden lg:block">
               O Nubank é uma fintech brasileira que oferece uma gama completa de serviços financeiros digitais, incluindo cartão de crédito, conta digital gratuita, empréstimos, seguros e investimentos. Fundado em 2013 para eliminar a burocracia, o Nubank é conhecido por sua simplicidade, transparência e por ser totalmente gerenciado via aplicativo móvel.
             </p>
-            <img src="imagens/quadrados.svg" alt="" className="w-sreen h-full fixed left-2" />
-            <img src="imagens/crianca.svg" alt="" className="fixed w-1/3 h-1/2 right-1/4 bottom-0" />
+            <img src="imagens/quadrados.svg" alt="" className="w-sreen h-full fixed left-2 hidden lg:block" />
+            <img src="imagens/crianca.svg" alt="" className="fixed w-1/3 h-1/2 right-1/4 bottom-0 hidden lg:block" />
           </div>
-          <div className="w-1/3 h-full bg-[#2e2d2d] flex flex-row justify-center items-start">
-            <form className="w-full max-w-md bg-white p-6 flex flex-col gap-4 shadow-lg overflow-y-auto max-h-[90vh] ">
-              <img src="imagens/pessoa.svg" alt="" className="w-36 h-36 mx-auto mb-2" />
+          <div className="w-full p-5 lg:w-1/3 min-h-screen bg-[#2e2d2d] flex justify-center items-center">
+            <form className="w-full max-w-md bg-white p-6 flex flex-col gap-4 shadow-lg overflow-y-auto max-h-[90vh]">
+
+              <div className=" flex justify-center items-center h-28 w-full">
+
+              <label htmlFor="fotoUpload" className="cursor-pointer">
+                <img
+                  src={foto || "imagens/pessoa.svg"}
+                  alt="Foto de perfil"
+                  className="w-48 h-48 rounded-full object-contain"
+                />
+              </label>
+
+              <input
+                  type="file"
+                  id="fotoUpload"
+                  accept="image/*"
+                  className="hidden"
+                  onChange={handleFotoChange}
+                />
+              </div>
 
               <h1 className="text-2xl font-bold text-center mb-2">Cadastro Cliente</h1>
 
@@ -27,7 +55,7 @@ export function Cadastro() {
                 <input type="text" placeholder="Nome completo" className="w-full bg-gray-100 focus:outline-none focus:ring-2 focus:ring-[#6dd63a] focus:border-transparent rounded-lg p-2" />
                 <div className="flex gap-2 mt-2">
                   <input type="text" placeholder="CPF" className="w-1/2 broder-2 bg-gray-100 focus:outline-none focus:ring-2 focus:ring-[#6dd63a] focus:border-transparent rounded-lg p-2 text-sm" />
-                  <input type="text" placeholder="Data Nasc." className="w-1/2 bg-gray-100 focus:outline-none focus:ring-2 focus:ring-[#6dd63a] focus:border-transparent rounded-lg p-2" />
+                  <input type="date" placeholder="Data Nasc." className="w-1/2 bg-gray-100 focus:outline-none focus:ring-2 focus:ring-[#6dd63a] focus:border-transparent rounded-lg p-2 text-gray-500" />
                 </div>
                 <div className="flex gap-2 mt-2">
                   <input type="text" placeholder="E-mail" className="w-1/2 bg-gray-100 focus:outline-none focus:ring-2 focus:ring-[#6dd63a] focus:border-transparent rounded-lg p-2" />
@@ -54,7 +82,7 @@ export function Cadastro() {
 
               <section>
                 <h3 className="font-semibold mt-4 mb-1">Chave Pix</h3>
-                <p className="text-sm text-gray-600 mb-2">Escolha como receber no MonoCoin</p>
+                <p className="text-sm text-gray-600 mb-5">Escolha como receber no MonoCoin</p>
 
                 <div className="flex flex-col gap-2">
                   {[
@@ -75,13 +103,13 @@ export function Cadastro() {
               </div>
               <button
                 type="submit"
-                className="w-10 h-10 rounded-full bg-[#00ec6a] hover:bg-[#00d65a] transition flex items-center justify-center rigth border-1 fixed top-5/6 left-11/12"
+                className="w-10 h-10 rounded-full bg-[#00ec6a] hover:bg-[#00d65a] transition flex items-center justify-center rigth border-1 fixed top-5/6 sm:left-11/12 left-80"
               >
                 <ArrowRight className="w-5 h-5 text-black left-14" />
               </button>
             </form>
 
-            <img src="imagens/ondas.svg" alt="" className="w-1/3 fixed top-3/4" />
+            <img src="imagens/ondas.svg" alt="" className="sm:w-1/3  w-full fixed top-3/4 sm:mt-5" />
           </div>
         </div>
 
--- Frontend/src/pages/Login.jsx
+++ Frontend/src/pages/Login.jsx
@@ -12,21 +12,25 @@ export function Login() {
               O Nubank é uma fintech brasileira que oferece uma gama completa de serviços financeiros digitais, incluindo cartão de crédito, conta digital gratuita, empréstimos, seguros e investimentos. Fundado em 2013 para eliminar a burocracia, o Nubank é conhecido por sua simplicidade, transparência e por ser totalmente gerenciado via aplicativo móvel.
             </p>
             <img src="imagens/quadrados.svg" alt="" className="w-sreen h-full fixed left-2 hidden lg:block" />
-            <img src="imagens/crianca.svg" alt="" className="fixed w-1/3 h-1/2 right-1/4 bottom-0 hidden lg:block" />
+            <img src="imagens/crianca.svg" alt="" className="fixed w-1/3 h-1/2 right-1/4 bottom-0 hidden lg:block"/>
           </div>
           <div className="w-full p-5 lg:w-1/3 min-h-screen bg-[#2e2d2d] flex justify-center items-center">
-            <form action="" method="" className="w-11/12 sm:w-3/4 h-full md:w-2/3 lg:w-5/6 bg-white rounded-2xl flex flex-col justify-center items-center p-6 shadow-lg">
-              <img src="imagens/seguranca.svg" alt="" className="sm:w-1/4 sm:h-1/4 h-44 w-44 fixed top-0"/>
+            <form action="" method="" className="w-11/12 sm:w-3/4 h-11/12 md:w-2/3 lg:w-5/6 bg-white rounded-2xl flex flex-col justify-center items-center p-6 shadow-lg">
+              <img src="imagens/seguranca.svg" alt="" className="sm:w-1/4 sm:h-1/4 h-44 w-44 fixed top-0 sm:top-10"/>
 
               <h1 className="text-2xl font-bold">Login</h1>
               <br/>
-              <label htmlFor="" className="w-3/4 p-2">Usuário</label>        
-              <input type="text" placeholder="Fulano de Tal" className="w-3/4 bg-gray-100 rounded-lg p-2 text-sm focus:outline-none focus:ring-2 focus:ring-[#6dd63a] focus:border-transparent" />  
+              <div className="flex flex-col">
+                <label htmlFor="" className="w-3/4 p-2">Usuário</label>        
+                <input type="text" placeholder="Fulano de Tal" className=" w-60 bg-gray-100 rounded-lg p-2 text-sm focus:outline-none focus:ring-2 focus:ring-[#6dd63a] focus:border-transparent" />  
+              </div>
+              <div className="flex flex-col m-5">
               <label htmlFor="" className=" p-2 w-3/4">Senha</label>
-              <input type="text" placeholder="*********" className=" w-3/4 bg-gray-100 rounded-lg p-2 text-sm focus:outline-none focus:ring-2 focus:ring-[#6dd63a] focus:border-transparent"/>
-              <button className="m-10 w-1/2 h-10 rounded-2xl bg-[#00ec6a]">Entrar</button>
+              <input type="text" placeholder="*****" className="w-60 bg-gray-100 rounded-lg p-2 text-sm focus:outline-none focus:ring-2 focus:ring-[#6dd63a] focus:border-transparent"/>
+              </div>
+              <button className="m-5 w-1/2 h-10 rounded-2xl bg-[#00ec6a]">Entrar</button>
             </form>
-            <img src="imagens/ondas.svg" alt="" className="w-1/3 fixed top-3/4 hidden lg:block" />
+            <img src="imagens/ondas.svg" alt="" className="sm:w-1/3 w-full fixed top-3/4" />
           </div>
         </div>
 
