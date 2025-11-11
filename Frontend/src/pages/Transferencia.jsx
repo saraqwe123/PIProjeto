cf629d8ace80eafb3b0725c3b3e7e84f565121d9
Lara Deitos <130068472+laraheloisa@users.noreply.github.com>
2025-11-11 00:49:40 -0300

    pagina area pix finalizada
    fazer as rotas na main das paginas tranferencia, mudar chave e pix copia e cola.

--- Frontend/src/pages/Transferencia.jsx
+++ Frontend/src/pages/Transferencia.jsx
@@ -1,8 +1,4 @@
 import { Pagina } from "../components/Pagina";
-import { MoveUpRight } from "lucide-react";
-import { Key } from "lucide-react";
-import { Copy } from "lucide-react";
-import { DollarSign } from "lucide-react";
 import { CircleArrowLeft } from "lucide-react";
 import { CircleQuestionMark } from "lucide-react";
 
@@ -35,42 +31,6 @@ export function AreaPix() {
         <main className="w-full flex-1 bg-[#259337] flex flex-col items-start p-10">
           <p className="text-white font-bold text-xl mb-6 ml-10">Área Pix</p>
 
-          <div className="flex gap-10 ml-10 flex-wrap">
-            <div className="flex flex-col items-center">
-              <button
-                onClick={() => navigate('/transferencia')}
-                className="group w-50 h-50 bg-[#c1ff72] rounded-2xl shadow-md flex justify-center items-center hover:bg-[#cfee9e] transition-colors"
-              >
-                <MoveUpRight className="w-20 h-50 transition-colors group-hover:text-[#4a8b00]" />
-              </button>
-              <p className="text-white mt-2">Transferência</p>
-            </div>
-
-            <div className="flex flex-col items-center">
-              <button
-                onClick={() => navigate('/mudarchave')}
-                className="group w-50 h-50 bg-[#5ce1e6] rounded-2xl shadow-md flex justify-center items-center hover:bg-[#cfee9e] transition-colors"
-              >
-                <Key className="w-20 h-50 transition-colors group-hover:text-[#4a8b00]" />
-              </button>
-              <p className="text-white mt-2">Mudar Chave</p>
-            </div>
-
-            <div className="flex flex-col items-center">
-              <button
-                onClick={() => navigate('/pixcopiaecola')}
-                className="group w-50 h-50 bg-gradient-to-r from-[#5170ff] to-[#ff66e4] rounded-2xl shadow-md flex justify-center items-center hover:from-[#a1ff72] hover:to-[#cfee9e] transition-colors"
-              >
-                <Copy className="w-20 h-50 transition-colors group-hover:text-[#4a8b00]" />
-              </button>
-              <p className="text-white mt-2">Pix Copia e Cola</p>
-            </div>
-
-            <div className="flex flex-col items-center">
-             <div className="w-50 h-50 bg-gradient-to-r from-[#0097b2] to-[#7ed957] rounded-2xl shadow-md flex justify-center items-center"><DollarSign className="w-20 h-50"/></div>
-              <p className="text-white mt-2">Cobrar</p>
-            </div>
-          </div>
         </main>
 
         <hr  className="h-1 bg-[#d9d9d9] border-[#d9d9d9]"/>
